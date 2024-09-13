7cc3526a2210d58d6d933c1ede43be03c555564c
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-09-13 07:57:29 -0700

    components add

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,46 @@ import TodoList from "./components/todoList";
 import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  // console.log("inputValue=>", inputValue);
 
-  const [inputValue, setInputValue] = useState('')
   const [Todos, setTodos] = useState([
     {
       todo: "testing",
       id: Date.now,
       completed: false,
-    }
+    },
   ]);
+  console.log("Todos", Todos);
+
+  const handleOnAddButton = () => {
+    const todosAdd = setTodos([
+      ...Todos,
+      {
+        todo: inputValue,
+        id: Date.now,
+        completed: false,
+      },
+    ]);
+    setTodos([...todosAdd]);
+    setInputValue("");
+
+    console.log("todosAdd Click");
+  };
 
   return (
     <div className="container-fluid">
       <div className="container">
-      <h2>Todo App</h2>
-      <TodoInput value={inputValue}/>
+        <h2>Todo App</h2>
+        <TodoInput
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onClick={handleOnAddButton}
+        />
 
-      <TodoList todos={Todos} />
-
+        <TodoList todos={Todos} />
       </div>
+      v
     </div>
   );
 }
--- src/components/todoInput.jsx
+++ src/components/todoInput.jsx
@@ -1,12 +1,27 @@
-function TodoInput({value}) {
-  return(
-     <>
-     <div>
-        <input type="text" placeholder="Add" className="myInput" value={value}/>
-        <button className="btn1">Add Todo</button>
+function TodoInput({ value, onChange, onClick }) {
+  return (
+    <>
+      <div className="todosBox">
+        <input
+          type="text"
+          placeholder="Add"
+          className="myInput"
+          value={value}
+          onChange={onChange}
+        />
+        <button
+          className="btn1"
+          disabled={value === ""}
+          style={{
+            backgroundColor: value === "" ? "hsl(224, 87%, 76%)" : "#000",
+          }}
+          onClick={onClick}
+        >
+          Add Todo
+        </button>
       </div>
-     </>
-   );
+    </>
+  );
 }
 
-export default TodoInput; 
+export default TodoInput;
